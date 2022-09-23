d6e9535ce2c577bbdd633f57517392b9288ea3ca
Esteban Sanjurjo <41419364+webma5ter23@users.noreply.github.com>
2022-09-23 04:44:03 -0300

    commit inicial

---
+++ public/notificaciones/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $fcm_server_key = 'AAAA_kA9GwE:APA91bFc48-q_viNhlsRdV9vpXVuIs4O4EnjIDMlYpiqtzRjy4XZ7DErXJFWwNxggX1GZjFdPKY2QbcTi0aVjlb-Gv1UBBTy2gTbVLWvmeXLJqSGtXYGloNIZvE1c5mnKpT1n0MaZB0L';
+
+    $fcm_topic = 'neri_radio_topic';
+
+    $onesignal_app_id = '2dbf9ff0-6345-49b5-bda5-2ee8c7cff510';
+    
+    $onesignal_rest_api_key = 'MWU0ZDVkODktZjI3Mi00ODE1LTgzYzItZWM2ZTQ3YjVmZmQ0';
+
+?>
